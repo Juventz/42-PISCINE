a10dce81a9c18e59843f9b124bb65f07a04cf01f
Juventz Aristil mathurin <jaristil@e3r9p12.clusters.42paris.fr>
2022-10-03 20:25:18 +0200

    mise a jour

--- EXAMEN FINAL ENTRAINEMENT/ex00/aff_a.c
+++ EXAMEN FINAL ENTRAINEMENT/ex00/aff_a.c
@@ -6,28 +6,34 @@
 /*   By: jaristil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/02 12:41:42 by jaristil          #+#    #+#             */
-/*   Updated: 2022/10/02 12:44:40 by jaristil         ###   ########.fr       */
+/*   Updated: 2022/10/03 15:40:58 by jaristil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	aff_a(char *str)
+int	main(int argc, char **argv)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0')
+	if (argc != 2)
 	{
 		write(1, "a\n", 2);
+		return (0);
+	}
+	while (argv[1][i])
+	{
+		if (argv[1][i] == 'a')
+		{
+			write(1, "a\n", 2);
+			return (0);
+		}
 		i++;
 	}
-}
-
-int	main(int argv, char **argc)
-{
-	(void)argc;
-	(void)argv;
-	write(1, "a\n", 2);
+	write(1, "\n", 1);
 	return (0);
 }
+/*
+Afficher a dans tout les cas hormi si 'a' n'appartient pas a str
+*/
--- EXAMEN FINAL ENTRAINEMENT/ex01/repeat_alpha.c
+++ EXAMEN FINAL ENTRAINEMENT/ex01/repeat_alpha.c
@@ -6,13 +6,13 @@
 /*   By: jaristil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/02 16:52:11 by jaristil          #+#    #+#             */
-/*   Updated: 2022/10/02 18:17:11 by jaristil         ###   ########.fr       */
+/*   Updated: 2022/10/03 17:00:27 by jaristil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	check_letter(char c)
+int	repeat_alpha(char c)
 {
 	int	i;
 
@@ -28,8 +28,24 @@ int	check_letter(char c)
 int	main(int argc, char **argv)
 {
 	int 	i;
+	int	index;
 
 	if (argc != 2)
 	{
-		write(1, 
-	}  
+		write(1, "\n", 1); 
+	}
+	while (argv[1][i])
+	{
+		index = repeat_alpha(argv[1][i]);
+		while (index--)
+		{
+			write(1, &argv[1][i], 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
+/*
+ne fonctionne pas
+*/
